898f882053e8eff5fcb6c99622adde47bed69be3
yixijin <jin.yixi@northeastern.edu>
2024-06-03 00:03:42 -0700

    create game maps and obstacles

---
+++ kob_web/src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import UserBotsIndexView from '../views/user/bots/UserBotsIndexView'
+import NotFound from '../views/error/NotFound'
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/user/bots/",
+    name: "user_bots_index",
+    component: UserBotsIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ kob_web/src/views/record/RecordIndexView.vue
@@ -0,0 +1,18 @@
+<template>
+    <ContentField>
+        Record
+    </ContentField>
+</template>
+
+<script>
+import ContentField from '../../components/ContentField.vue'
+
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+
+<style scoped></style>
